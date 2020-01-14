9dc5fb7dba0858d11944bbd5001010de3ec605a1
Shaina C <scailles23@gmail.com>
2020-01-14 03:28:21 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+  
+  def initialize(name)
+    @name= name
+  end
+  
+  def name
+    @name
+  end
+  
+  def name(new_name)
+    @name=new_name
+  end
+  
+  def breed=(dogs_breed)
+    dogs_breed=breed
+    @breed=dogs_breed
+  end
+  
+  def breed
+    @dogs_breed
+  end
+  
+end
+
+Dog.new("fido")
